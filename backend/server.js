d549ca30d4659fe0273e2630410a1f31aa0dca4f
Adrian <adrianhenry2115@gmail.com>
2023-10-03 10:55:32 -0400

    starting to hash passwords, server currently working correctly

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 import express from 'express';
-const port = 5000;
+import dotenv from 'dotenv'
+dotenv.config();
+import connectDB from './config/db.js'
+// routes
+import userRoutes from './routes/userRoutes.js'
+// error middlewares
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+connectDB();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+// parse data to json
+app.use(express.json());
+// parse form data
+app.use(express.urlencoded({ extended: true }))
+
+// route usage
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('Server is ready'));
+
+// error middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
