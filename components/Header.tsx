d34c5bd747da1bcea69cebce15c3413cc77fba0b
Adrian <adrianhenry2115@gmail.com>
2023-11-10 15:31:54 -0500

    video editted and header styles fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,22 +11,22 @@ import Logo from "../public/taharka_logo.png";
 
 const Header = () => {
     return (
-        <div className="flex items-center justify-between w-full px-4 fixed top-0 bg-white z-10 rounded-b-lg lg:w-2/3">
+        <div className="flex items-center justify-between w-full px-4 fixed top-0 bg-white z-10 rounded-b-lg lg:w-4/5">
             {/* HAMBURGER AND LOGO CONTAINER  */}
-            <div className="flex items-center justify-start">
-                <RxHamburgerMenu className="text-zinc-500 font-extrabold" size={30} onClick={() => {}} />
-                <Link className="pl-10 flex items-center" href={"/"}>
-                    <Image className="pb-4" width={50} src={Logo} alt="logo" />
-                    <span className="font-bold text-2xl">Taharka Bros.</span>
+            <div className="flex items-center w-full">
+                <RxHamburgerMenu className="text-zinc-500 font-extrabold fixed left-5 md:relative" size={30} onClick={() => {}} />
+                <Link className="pl-10 flex items-center w-full justify-center md:justify-start" href={"/"}>
+                    <Image className="pb-4" width={40} src={Logo} alt="logo" />
+                    <span className="font-bold text-md text-center">Taharka Bros.</span>
                 </Link>
             </div>
             {/* LOCATION AND ORDER NOW CONTAINER */}
-            <div className="flex items-center">
+            <div className="items-center hidden md:flex">
                 <Link className="flex items-center p-4" href={"/location"}>
                     <MdLocationPin size={20} className="mr-2" />
-                    <span>Find A Shop</span>
+                    <span className="whitespace-nowrap">Find A Shop</span>
                 </Link>
-                <Link className="bg-black rounded-full text-white px-10 py-2" href={"/order"}>
+                <Link className="bg-black rounded-full text-white px-10 py-2 whitespace-nowrap" href={"/order"}>
                     <span className="font-bold">Order Now</span>
                 </Link>
             </div>
--- frontend/src/pages/order/ChooseOrder.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChooseOrder = () => {
-    return <div>ChooseOrder</div>;
-};
-
-export default ChooseOrder;
