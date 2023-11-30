149cf0955751f06b32b413f3c965b84ee6dab098
Adrian <adrianhenry2115@gmail.com>
2023-11-29 19:51:45 -0500

    some product stuff and modal stuff

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { Fragment, useEffect, useRef, useState } from "react";
+
 import { Dialog, Transition } from "@headlessui/react";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
 import Price from "@/components/price";
 import { DEFAULT_OPTION } from "@/lib/constants";
 import type { Cart } from "@/lib/shopify/types";
 import { createUrl } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { Fragment, useEffect, useRef, useState } from "react";
+
 import CloseCart from "./close-cart";
 import { DeleteItemButton } from "./delete-item-button";
 import { EditItemQuantityButton } from "./edit-item-quantity-button";
--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -9,7 +9,7 @@ import { getCartQuery } from "./queries/cart";
 import { getCollectionProductsQuery, getCollectionQuery, getCollectionsQuery } from "./queries/collection";
 import { getMenuQuery } from "./queries/menu";
 import { getPageQuery, getPagesQuery } from "./queries/page";
-import { getProductQuery, getProductRecommendationsQuery, getProductsQuery } from "./queries/product";
+import { getProductMetafieldsQuery, getProductQuery, getProductRecommendationsQuery, getProductsQuery } from "./queries/product";
 import {
     Cart,
     Collection,
